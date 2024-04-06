72ba4478d1749a20b708855d7ac2e41a56912d98
Anna Piaskovska <piaskovska.pn@ucu.edu.ua>
2024-04-06 13:42:47 +0300

    task "Linked Lists - Move Node" done

---
+++ Linked Lists - Move Node/move_node.py
@@ -0,0 +1,13 @@
+class Node():
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+    
+class Context(object):
+    def __init__(self, source, dest):
+        self.source = source
+        self.dest = dest
+    
+def move_node(source, dest):
+    dest = Node(source.data, dest)
+    return Context(source.next, dest)
